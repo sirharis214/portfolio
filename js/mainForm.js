0dc65ae0f2ee5f25f60c288aef077141410f4a63
sirharis214 <36185908+sirharis214@users.noreply.github.com>
2022-02-27 14:56:47 -0500

    form validation

--- js/mainForm.js
+++ js/mainForm.js
@@ -1,35 +1,43 @@
+// Functions
 let id = (id) => document.getElementById(id);
 let classes = (classes) => document.getElementsByClassName(classes);
 
+// Store variables
 let username = id("username"),
-errorMsg = classes("error"),
 form = id("mainForm"),
-successIcon = classes("fa-check"),
-failIcon = classes("fa-exclamation");
 
-form.addEventListener("submit", (e)=>{
+errorMsg = classes("error"),
+successIcon = classes("success-icon"),
+failureIcon = classes("failure-icon");
+
+// Submit event listener        
+form.addEventListener("submit", (e) =>{
     e.preventDefault();
     engine(username, 0, "Username cannot be blank");
-    //engine(email, 1, "Email cannot be blank");
+    // engine(email, 1, "Email cannot be blank");
 });
 
-/*  id will target our id
-    serial will target our classes [error class, success and failure icons]
-    message will print a message inside our .error class 
-*/
+// Module
+// id will target our id
+// serial will target our classes [error class, success and failure icons]
+// message will print a message inside our .error class 
 let engine = (id, serial, message) => {
+    // .trim() to remove white spaces
     if (id.value.trim() === ""){
         errorMsg[serial].innerHTML = message;
-        id.style.border = "2px solid red";
+        // id.style.border = "2px solid red";
+        id.style.border = "2px solid #ff0000";
         // icons
-        failIcon[serial].style.opacity = "1";
+        failureIcon[serial].style.opacity = "1";
         successIcon[serial].style.opacity = "0";
     } 
-    else{
+    
+    else {
         errorMsg[serial].innerHTML = "";
-        id.style.border = "2px solid green";
+        // id.style.border = "2px solid green";
+        id.style.border = "2px solid #00b300";
         // icons
-        failIcon[serial].style.opacity = "0";
+        failureIcon[serial].style.opacity = "0";
         successIcon[serial].style.opacity = "1";
     }
 }
